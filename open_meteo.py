b5b74d2370b8838428fb00d0677565ec5a17dd0c
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-09-24 11:16:07 -0300

    feat:add function to get temperature over a date range

--- open_meteo.py
+++ open_meteo.py
@@ -5,14 +5,14 @@ from requests import Response
 url_forecast = "https://api.open-meteo.com/v1/forecast"
 
 # Función para obtener la temperatura en un rango de fechas
-#def get_temperature_by_dates(lat: float, lon: float, date_i: str, date_f: str) -> Response:
-#    params = {
-#        "latitude": lat,
-#        "longitude": lon,
-#        "hourly": "temperature_2m",
-#        "start_date": date_i,
-#        "end_date": date_f,
-#        "timezone": "UTC"
-#    }
-#    response: Response = requests.get(url_forecast, params=params)
-#    return response
+def get_temperature_by_dates(lat: float, lon: float, date_i: str, date_f: str) -> Response:
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": "temperature_2m",
+        "start_date": date_i,
+        "end_date": date_f,
+        "timezone": "UTC"
+    }
+    response: Response = requests.get(url_forecast, params=params)
+    return response
