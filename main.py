ca2d0487b1a4202dfddfc6599714a969da25d1c6
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-09-24 12:47:30 -0300

    chore:add library request and Respnse

---
+++ main.py
@@ -0,0 +1,41 @@
+import uvicorn
+import requests
+
+from fastapi import FastAPI, Query
+from requests import Response
+
+app = FastAPI()
+
+#@app.get("/temperature")
+#def get_temperature_by_dates(lat: float, lon: float, date_i: str, date_f: str) -> Response:
+#    params = {
+#        "latitude": lat,
+#        "longitude": lon,
+#        "hourly": "temperature_2m",
+#        "start_date": date_i,
+#        "end_date": date_f,
+#        "timezone": "UTC"
+#    }
+#    response: Response = requests.get(url_forecast, params=params)
+#    return response
+
+# @app.get("/ping")
+# def ping():
+#     result = {"Eve": "Vieja"}
+#     return result
+
+# @app.get("/dummy_endpoint")
+# def dummy_endpoint(param_1: str = Query(...), param_2: int = Query(...)):
+#    print(f"param_1={param_1} - param_2={param_2}")
+#    return {"content": "dummy"}
+#
+# def my_complex_function(a: int, b: int):
+#    return a + b
+#
+# @app.get("/some_new_endpoint")
+# def some_new_endpoint(a: int, b: int):
+#    result = my_complex_function(a, b)
+#    return {"content": result}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
