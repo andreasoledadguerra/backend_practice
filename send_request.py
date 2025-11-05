19237f736b7dd5163cadc42c509f0ccd3e488325
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-04 22:59:56 -0300

    feat: add request for weather by city

--- send_request.py
+++ send_request.py
@@ -16,10 +16,17 @@ if __name__ == "__main__":
     print(response.status_code)
     print(response.json())
 
-    #params = {"param_1": "test", "param_2": 123}
-    #response = requests.get("http://localhost:8000/dummy_endpoint", params=params)
-    #print(response.status_code)
-    #print(response.json())
+    params = {
+        "city": str,
+        "lat": 40.7128,
+        "lon": -74.0060,
+        "current_weather": "true",
+        "timezone": "UTC"
+    }
+        
+    response = requests.get("http://localhost:8000/temperature/{city}", params=params)
+    print(response.status_code)
+    print(response.json())
 #
     #response = requests.get("http://localhost:8000/some_new_endpoint", params={"a": 5, "b": 10})
     #print(response.status_code)
