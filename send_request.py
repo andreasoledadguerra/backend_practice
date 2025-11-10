2b768b3b462e75257526126055871c38486e2bdd
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-10 11:23:02 -0300

    refactor: update date range

--- send_request.py
+++ send_request.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     params = {
         "lat": 40.7128,
         "lon": -74.0060,
-        "date_i": "2025-08-01",
-        "date_f": "2025-08-07"
+        "date_i": "2025-08-09",
+        "date_f": "2025-08-15"
     }
     response = requests.get("http://localhost:8000/temperature", params=params)
     print(response.status_code)
